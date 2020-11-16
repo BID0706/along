0198a240cf23318f1cdf4c5b82c8e8eba839d0ff
unknown <ibiso.barimalaa@steadsmart.com>
2020-11-16 11:40:23 +0100

    Updates

---
+++ precache-manifest.65c86cef09d1a1f594a61bb13a58216b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e320f5836ddf3d01c13b5dd80fc5238",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e190e132d1e372905476",
+    "url": "/static/css/2.06890bda.chunk.css"
+  },
+  {
+    "revision": "8a66c5300a31f3a3f4c9",
+    "url": "/static/css/main.af59ab7a.chunk.css"
+  },
+  {
+    "revision": "e190e132d1e372905476",
+    "url": "/static/js/2.11812359.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.11812359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a66c5300a31f3a3f4c9",
+    "url": "/static/js/main.33cdd8d2.chunk.js"
+  },
+  {
+    "revision": "f1b171453e7263b9c91c",
+    "url": "/static/js/runtime-main.2bb3d4fb.js"
+  },
+  {
+    "revision": "8f850222ac30475b1021cfd73259a0c3",
+    "url": "/static/media/app.8f850222.png"
+  },
+  {
+    "revision": "d0bc1989f1432b969a6602ce2edf3926",
+    "url": "/static/media/car.d0bc1989.jpg"
+  },
+  {
+    "revision": "0f154eff757a902e964cee5dc43f98fa",
+    "url": "/static/media/img1.0f154eff.png"
+  },
+  {
+    "revision": "fbc3c6773ad537eefc14179788b06777",
+    "url": "/static/media/img2.fbc3c677.png"
+  },
+  {
+    "revision": "a1b7302fd06df33d19b5552185f83507",
+    "url": "/static/media/mac.a1b7302f.png"
+  },
+  {
+    "revision": "ef8ea63800f974b0d6f449d3e8cd5b50",
+    "url": "/static/media/man-in-car.ef8ea638.jpg"
+  },
+  {
+    "revision": "0772dbbb722b17b741eba89221767519",
+    "url": "/static/media/man-texting.0772dbbb.jpg"
+  },
+  {
+    "revision": "5b609bd2f26236007db00a3dde7bdda5",
+    "url": "/static/media/woman-driving.5b609bd2.jpeg"
+  }
+]);
